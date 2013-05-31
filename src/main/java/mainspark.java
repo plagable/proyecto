db8d8df9b0f113806fb4dc3bfe091e1ac6586547
plagable <patolagable@gmail.com>
2013-05-31 02:39:19 +0200

    prueba de spark

--- src/main/java/mainspark.java
+++ src/main/java/mainspark.java
@@ -1,18 +1,35 @@
+
+import com.unrc.app.models.Owner;
+import spark.*;
 import static spark.Spark.*;
 
-import spark.*;
-import spark.Route;
+
 public class mainspark {
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      get(new Route("/hello") {
-         @Override
-         public Object handle(Request request, Response response) {
-            return "Hello World!";
-         }
-      });
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                Owner todosLosOwner = Owner.findFirst("first_name=pato");
+                return "Hello World!";       
+            }
+        });
 
-   }
-
+        get(new Route("/owners") {
+            @Override
+            public Object handle(Request request, Response response) {
+                 String aux= "";
+                 Owner todosLosOwner = Owner.findById(1);
+                 
+//                 for (Owner o: ob) {
+//                     aux.concat(o.toString());
+//                 }
+                 //return aux;
+                 return "hola";
+             }
+             
+            });
+     
+}
 }
